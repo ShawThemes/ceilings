099311a39c408895a4cc6b943462ae35918f990d
rodionov217 <rodionova217@gmail.com>
2020-06-01 20:42:44 +0300

    services slider

--- about-slider.js
+++ about-slider.js
@@ -1,10 +1,29 @@
 'use strict';
 
-$('.owl-carousel').owlCarousel({
+$('.about-slider.owl-carousel').owlCarousel({
   loop:true,
   margin:0,
   nav:false,
   dots:false,
   items: 1,
   autoplay: true
+});
+
+$('.employees-list.owl-carousel').owlCarousel({
+  loop:true,
+  margin:20,
+  nav:false,
+  dots:true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 3
+    }
+  },
+  autoplay: false
 })
--- main-slider.js
+++ main-slider.js
@@ -1,10 +1,29 @@
 'use strict';
 
-$('.owl-carousel').owlCarousel({
+$('.main-slider.owl-carousel').owlCarousel({
   loop:true,
   margin:0,
   nav:false,
   dots:true,
   items: 1,
   autoplay: true
+})
+
+
+$('.services-list.owl-carousel').owlCarousel({
+  loop:true,
+  margin:20,
+  nav:false,
+  dots:true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 3
+    }
+  }
 })
