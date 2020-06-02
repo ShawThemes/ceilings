2533afd50885eb9f5a7630feac32949b786f2f85
ShawThemes <thehenryshaw@gmail.com>
2020-06-02 12:18:57 +0300

    responsive 1000

--- main.js
+++ main.js
@@ -28,18 +28,3 @@ const workSectionBanner = document.querySelector('.work-bcg__image');
 new simpleParallax(workSectionBanner, {
   scale: 1.8
 });
-
-
-/* MENU BUTTON */
-const menuBtn = document.querySelector('.menu-button');
-const menu = document.querySelector('nav');
-
-menuBtn.addEventListener('click', event => {
-  menuBtn.classList.toggle('close');
-  menu.classList.toggle('show');
-  if (event.currentTarget.classList.contains('close')) {
-    document.body.parentNode.style.overflowY = 'hidden';
-  } else {
-    document.body.parentNode.style.overflowY = 'visible';
-  }
-}); 
---
+++ menu.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* MENU BUTTON */
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', event => {
+  menuBtn.classList.toggle('close');
+  menu.classList.toggle('show');
+  if (event.currentTarget.classList.contains('close')) {
+    document.body.parentNode.style.overflowY = 'hidden';
+  } else {
+    document.body.parentNode.style.overflowY = 'visible';
+  }
+}); 
