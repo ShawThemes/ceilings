3048e082baeeffa7e69ad802aeef5e052d0b7e98
rodionov217 <rodionova217@gmail.com>
2020-06-06 19:06:08 +0300

    multiple changes

---
+++ mail.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['firstName'];
+$phone = $_POST['phone'];
+
+// Формирование самого письма
+$title = "Заголовок письма";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Почта:</b> $phone<br><br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'henryshaw217'; // Логин на почте
+    $mail->Password   = '92930709mother'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('henryshaw217@yandex.ru', 'ShawThemes'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('henryshaw217@yandex.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+?>
--- main.js
+++ main.js
@@ -1,30 +1,68 @@
 'use strict';
 
-/* INCREMENTING */
-const achievements = document.querySelector('.achievements');
+/* INCREMENTING EFFECT*/
 function countup(className){
-  let achievements = $("."+className).offset().top;
-  let windowHeight = window.innerHeight;
-  let show = true;
-        
-  jQuery(window).scroll( function (){
-    if(show && (achievements < $(window).scrollTop() + windowHeight)){ 
-      show = false;
-      jQuery('.'+className).spincrement({
-        from: 1,
-        duration: 5000,
-
-      });
-    }
-  })
+  let element = $("."+className);
+  if (element) {
+    let achievements = element.offsetTop;
+    let windowHeight = window.innerHeight;
+    let show = true;
+          
+    jQuery(window).scroll( function (){
+      if(show && (achievements < $(window).scrollTop() + windowHeight)){ 
+        show = false;
+        jQuery('.'+className).spincrement({
+          from: 1,
+          duration: 5000,
+        });
+      }
+    })
+  }
 }
 
-jQuery(function() {
+document.addEventListener('DOMContentLoaded', () => {
   countup("count", $(".count").text());
 });
 
+
 /* PARALLAX EFFECT FOR HOME SECTION */
 const workSectionBanner = document.querySelector('.work-bcg__image');
-new simpleParallax(workSectionBanner, {
-  scale: 1.8
+if (workSectionBanner) {
+  new simpleParallax(workSectionBanner, {
+    scale: 1.8
+  });
+}
+
+/* SUBMIT FORM */
+const submitForm = document.querySelector('#submitForm');
+
+submitForm.addEventListener('submit', event => {
+  let url = event.currentTarget.classList.contains('homepage-form') ? 'mail.php' : '../mail.php';
+  send(event , url);
+});
+
+function send(event, php){
+  event.preventDefault ? event.preventDefault() : event.returnValue = false;
+  var req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function() {
+	if (req.status >= 200 && req.status < 400) {
+	let json = JSON.parse(this.response);
+    	console.log(json);
+      if (json.result == "success") {
+    	alert("Сообщение отправлено");
+    } else {
+    	alert("Ошибка. Сообщение не отправлено");
+    	}
+    } else {alert("Ошибка сервера. Номер: "+req.status);}};
+req.onerror = function() {alert("Ошибка отправки запроса");};
+req.send(new FormData(event.target));
+}
+
+
+/* INPUT MASK */
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.getElementById("inputmask");
+  const im = new Inputmask("+7 (999)-999-9999");
+  im.mask(element);
 });
